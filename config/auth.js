e021e29e0a990590e9c64afd2522509a9f0ca331
QuintonWeenink <qweenink@gmail.com>
2016-05-26 20:45:15 +0200

    Changed the port and the callback for hosting

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'fb': {
         'appId': '959936647438530',
         'appSecret': 'a1b4088df1dc99305fee1fa09c2c6e61',
-        'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL': 'https://insuranceprofiling.herokuapp.com/auth/facebook/callback'
     }
 }
