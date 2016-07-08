1d2e1d96cd0ccb8d13f2f93c6438cde3f5adf16b
Quinton Weenink <qweenink@gmail.com>
2016-07-08 16:36:19 +0200

    Unit test for create user

--- test/test.js
+++ test/test.js
@@ -1,7 +1,21 @@
+var userController = require("../DBControllers/UserController");
+
 var assert = require('chai').assert;
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('User test', function() {
+  describe('Testing creat user()', function() {
+    it('should return user when all values not present', function() {
+      var newUser = {
+				firstName: 'Unit',
+				lastName: 'Test',
+				contactNumber : '0121212',
+				mobileNumber : '09312123',
+				maritalStatus : 'Married',
+				dateOfBirth : '1994/01/01 20:00',
+				gender : 'male',
+				location : 'Test',
+				email : 'email@email.com'
+		};
+			userController.createUser(newUser);
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
