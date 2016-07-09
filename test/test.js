a06671aa8030ad8051a4c98fcbb42d878afb620e
Quinton Weenink <qweenink@gmail.com>
2016-07-09 12:36:01 +0200

    Attempt at testing api calls using http request

--- test/test.js
+++ test/test.js
@@ -1,27 +1,37 @@
 var userController = require("../DBControllers/UserController");
+var request = require("request");
+
+function httpRequest(done)
+{
+
+    request('http://www.google.com', function (error, response, body, done) {
+    if (!error && response.statusCode == 200) {
+    console.log(body); // Show the HTML for the Google homepage.
+    done(body);
+    }
+    else {
+      console.log("error");
+      done("error");
+    }
+    });
+console.log("done"); // Show the HTML for the Google homepage.
+}
+
+function completed(comp)
+{
+    console.log(comp);
+}
 
 var assert = require('chai').assert;
 describe('User test', function() {
   describe('Testing creatUser(User)', function() {
     it('should not insert due to no connection', function() {
-      var newUser = {
-				firstName: 'Unit',
-				lastName: 'Test',
-				contactNumber : '0121212',
-				mobileNumber : '09312123',
-				maritalStatus : 'Married',
-				dateOfBirth : '1994/01/01 20:00',
-				gender : 'male',
-				location : 'Test',
-				email : 'email@email.com'
-		};
-			var addedUser = userController.createUser(newUser);
-      console.log("Exception: User not added due to no db connection")
+      httpRequest(completed);
     });
   });
 
   describe('Testing creatUser(User)', function() {
-    it('should return exception', function() {
+    it('should return exc', function() {
       var newUser = {
 				firstName: 'Unit',
 				lastName: 'Test',
