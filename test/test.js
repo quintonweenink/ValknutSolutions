ca37c3f7f6f93627e62b5a049c1041621905c04e
Quinton Weenink <qweenink@gmail.com>
2016-07-09 11:15:30 +0200

    Added db connection test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var userController = require("../DBControllers/UserController");
 var assert = require('chai').assert;
 describe('User test', function() {
   describe('Testing creatUser(User)', function() {
-    it('should return user when all values not present', function() {
+    it('should not insert due to no connection', function() {
       var newUser = {
 				firstName: 'Unit',
 				lastName: 'Test',
@@ -16,8 +16,25 @@ describe('User test', function() {
 				email : 'email@email.com'
 		};
 			var addedUser = userController.createUser(newUser);
-      console.log("after createUser: " + newUser.firstName);
-      //assert.equal(addedUser.dataValues, newUser.dataValues);
+      console.log("Exception: User not added due to no db connection")
+    });
+  });
+
+  describe('Testing creatUser(User)', function() {
+    it('should return exception', function() {
+      var newUser = {
+				firstName: 'Unit',
+				lastName: 'Test',
+				contactNumber : '0121212',
+				mobileNumber : '09312123',
+				maritalStatus : 'Married',
+				dateOfBirth : '1994/01/01 20:00',
+				gender : 'male',
+				location : 'Test',
+				email : 'email@email.com'
+		};
+			var addedUser = userController.createUser(newUser);
+      console.log("Exception: User not added due to no db connection")
     });
   });
 });
