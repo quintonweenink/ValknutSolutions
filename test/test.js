7c73307e8d750a77397b33331b1ad20a343bd06a
Quinton Weenink <qweenink@gmail.com>
2016-07-11 14:20:07 +0200

    api worked for about 5 min ...

--- test/test.js
+++ test/test.js
@@ -1,50 +1,28 @@
 var userController = require("../DBControllers/UserController");
-var request = require("request");
-
-function httpRequest(done)
-{
-
-    request('http://www.google.com', function (error, response, body, done) {
-    if (!error && response.statusCode == 200) {
-    console.log(body); // Show the HTML for the Google homepage.
-    done(body);
-    }
-    else {
-      console.log("error");
-      done("error");
-    }
-    });
-console.log("done"); // Show the HTML for the Google homepage.
-}
-
-function completed(comp)
-{
-    console.log(comp);
-}
-
+var request = require("supertest");
 var assert = require('chai').assert;
+
+var assert = require('assert');
+
 describe('User test', function() {
+
+
   describe('Testing creatUser(User)', function() {
-    it('should not insert due to no connection', function() {
-      httpRequest(completed);
+    it('should return exc', function(done) {
+
+      request("https://insuranceprofiling.herokuapp.com")
+	.post('/api/user')
+	.send('')
+    // end handles the response
+	.end(function(err, res) {
+          if (err) {
+            console.log("error");
+            throw err;
+          }
+          console.log(res.body);
+          done();
+        });
     });
   });
 
-  describe('Testing creatUser(User)', function() {
-    it('should return exc', function() {
-      var newUser = {
-				firstName: 'Unit',
-				lastName: 'Test',
-				contactNumber : '0121212',
-				mobileNumber : '09312123',
-				maritalStatus : 'Married',
-				dateOfBirth : '1994/01/01 20:00',
-				gender : 'male',
-				location : 'Test',
-				email : 'email@email.com'
-		};
-			var addedUser = userController.createUser(newUser);
-      console.log("Exception: User not added due to no db connection")
-    });
-  });
 });
