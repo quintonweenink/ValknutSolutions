8a6dbd30f9adea02fd2262db9086aa9153896136
BernhardSchuld <bernhardschuld@gmail.com>
2016-07-11 12:38:32 +0200

    Modified routes.js to work for analysts
    Signed-off-by: BernhardSchuld <bernhardschuld@gmail.com>

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ var models = require("../models");
 
 var userController = require("../DBControllers/UserController");
 var adminController = require("../DBControllers/AdminController");
+var analystController = require("../DBControllers/AnalystController");
 var email = require("../email/email");
 var jwt = require('jsonwebtoken');
 
@@ -185,6 +186,30 @@ next();
 	//User Get route
     .get(function(req, res) {
 			models.Admin.findAll().then(function(users){
+				res.json(users);
+		});
+		//Logic for returning all users
+	});
+
+
+	app.route('/api/analyst')
+	//User Post route
+    .post(function(req, res) {
+			var newAnalyst = {
+				email: 'a',
+				password: 'lelelel'
+		};
+			analystController.createAnalyst(newAnalyst)
+			.then(function(analyst){
+        res.json(analyst.dataValues);
+    }) .catch(function(error){
+         console.log("ops: " + error);
+         res.status(500).json({ error: 'error' });
+     });
+	})
+	//User Get route
+    .get(function(req, res) {
+			models.Analyst.findAll().then(function(users){
 				res.json(users);
 		});
 		//Logic for returning all users
