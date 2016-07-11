30bc93c26c61cc51dd2cb4add0256da6b0baae7d
Quinton Weenink <qweenink@gmail.com>
2016-07-11 16:08:42 +0200

    api testing updated

--- test/test.js
+++ test/test.js
@@ -4,25 +4,76 @@ var assert = require('chai').assert;
 
 var assert = require('assert');
 
-describe('User test', function() {
+var env = process.env.NODE_ENV || "development";
 
+var url;
+var path;
+var currentUser;
 
-  describe('Testing creatUser(User)', function() {
-    it('should return exc', function(done) {
+if (env != "development")
+{
+  url = "https://insuranceprofiling.herokuapp.com";
+}
+else {
+  url = "localhost";
+}
 
-      request("https://insuranceprofiling.herokuapp.com")
-	.post('/api/user')
-	.send('')
-    // end handles the response
-	.end(function(err, res) {
-          if (err) {
-            console.log("error");
-            throw err;
-          }
-          console.log(res.body);
-          done();
+describe("Testing API", function() {
+
+  path = "/api/user/";
+  describe("URL: " + url + path, function() {
+
+    describe('POST : /api/user/', function() {
+      it('should insert and return the inserted user', function(done) {
+        	//POST request
+        request(url)
+      	.post(path)
+      	.send('')
+      	.end(function(err, res) {
+                if (err) {
+                  console.log("error");
+                  throw err;
+                }
+                currentUser = res.body;
+                done();
         });
+      });
     });
-  });
 
+    describe("GET : /api/user/:currentUser", function() {
+      it('should get user with id', function(done) {
+        	//DELETE request
+        request(url)
+      	.get(path + currentUser.id)
+      	.send('')
+      	.end(function(err, res) {
+                if (err) {
+                  console.log("error");
+                  throw err;
+                }
+                assert.equal(res.body.id, currentUser.id);
+                done();
+        });
+      });
+    });
+
+    describe('DELETE : /api/user/:currentUser', function() {
+      it('should delete the just inserted user', function(done) {
+        	//DELETE request
+        request(url)
+      	.delete('/api/user/' + currentUser.id)
+      	.send('')
+      	.end(function(err, res) {
+                if (err) {
+                  console.log("error");
+                  throw err;
+                }
+                assert.equal(res.body.ID, currentUser.id);
+                done();
+        });
+      });
+    });
+
+
+  });
 });
