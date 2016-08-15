7b659abda82e19b1d2b3feabc49ba9740b9175ca
Charl Jansen van Vuuren <u13054903@tuks.co.za>
2016-08-15 20:25:32 +0200

    Added Graph example with libraries. Removed valuables page and added Graph page

--- public/js/script.js
+++ public/js/script.js
@@ -1,83 +1,97 @@
-	var InsuranceProfiling = angular.module('InsuranceProfiling', ['ngRoute']);
+var InsuranceProfiling = angular.module('InsuranceProfiling', ['ngRoute','chart.js']);
+// configure our routes
+InsuranceProfiling.config(function($routeProvider) {
+	$routeProvider
 
-	// configure our routes
-	InsuranceProfiling.config(function($routeProvider) {
-		$routeProvider
+	// route for the home page
+	.when('/', {
+		templateUrl : 'views/pages/home.html',
+		controller  : 'mainController'
+	})
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'views/pages/home.html',
-				controller  : 'mainController'
-			})
+	// route for the about page
+	.when('/about', {
+		templateUrl : 'views/pages/about.html',
+		controller  : 'aboutController'
+	})
 
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'views/pages/about.html',
-				controller  : 'aboutController'
-			})
+	// route for the contact page
+	.when('/login', {
+		templateUrl : 'views/pages/login.html',
+		controller  : 'loginController'
+	})
+	//route for login_form
+	.when('/login_form',
+	{
+		templateUrl : 'views/pages/login_form.html',
+		controller  : 'loginFormController'
+	})
+	//signup route
+	.when('/signup',
+	{
+		templateUrl : 'views/pages/signup.html',
+		controller  : 'signupController'
+	})
+	//"valuables" test route
+	.when('/graph',
+	{
+		templateUrl : 'views/pages/graph.html',
+		controller  : 'graphController'
+	})
 
-			// route for the contact page
-			.when('/login', {
-				templateUrl : 'views/pages/login.html',
-				controller  : 'loginController'
-			})
-			//route for login_form
-			.when('/login_form',
-			{
-				templateUrl : 'views/pages/login_form.html',
-				controller  : 'loginFormController'
-			})
-			//signup route
-			.when('/signup',
-			{
-				templateUrl : 'views/pages/signup.html',
-				controller  : 'signupController'
-			})
-			//"valuables" test route
-			.when('/valuables',
-			{
-				templateUrl : 'views/pages/valuables.html',
-				controller  : 'valuablesController'
-			})
+	;
+});
 
-			;
-	});
+// create the controller and inject Angular's $scope
+InsuranceProfiling.controller('mainController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Main Controller';
+});
 
-	// create the controller and inject Angular's $scope
-	InsuranceProfiling.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Main Controller';
-	});
+InsuranceProfiling.controller('aboutController', function($scope) {
+	$scope.message = 'Some information';
+});
 
-	InsuranceProfiling.controller('aboutController', function($scope) {
-		$scope.message = 'Some information';
-	});
+InsuranceProfiling.controller('loginController', function($scope) {
+	$scope.message = 'Login Page My friends';
+});
 
-	InsuranceProfiling.controller('loginController', function($scope) {
-		$scope.message = 'Login Page My friends';
-	});
+InsuranceProfiling.controller('loginFormController', function($scope) {
+	$scope.message = 'Error will be shown here my friends';
+});
 
-	InsuranceProfiling.controller('loginFormController', function($scope) {
-		$scope.message = 'Error will be shown here my friends';
-	});
+InsuranceProfiling.controller('signupController', function($scope) {
+	$scope.message = '';
+});
 
-	InsuranceProfiling.controller('signupController', function($scope) {
-		$scope.message = '';
-	});
-
-	InsuranceProfiling.controller('valuablesController', function($scope) {
-		$scope.message = '';
-		$scope.valuables =
-		[{
-          name: 'Handbag',
-          type: 'Gucci'
-        },
-        {
-          name: 'Mobile phone',
-          type: 'Android'
-        },
-        {
-          name: 'Tablet',
-          type: 'iPad'
-        }];
-	});
+InsuranceProfiling.controller('graphController', function($scope) {
+	$scope.message = '';
+	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+	$scope.series = ['Series A', 'Series B'];
+	$scope.data = [
+		[65, 59, 80, 81, 56, 55, 40],
+		[28, 48, 40, 19, 86, 27, 90]
+	];
+	$scope.onClick = function (points, evt) {
+		console.log(points, evt);
+	};
+	$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+	$scope.options = {
+		scales: {
+			yAxes: [
+				{
+					id: 'y-axis-1',
+					type: 'linear',
+					display: true,
+					position: 'left'
+				},
+				{
+					id: 'y-axis-2',
+					type: 'linear',
+					display: true,
+					position: 'right'
+				}
+			]
+		}
+	};
+});
