cef9831fcf4b4fa7f76cacece77e38ac9cb7d6f5
Charl Jansen van Vuuren <u13054903@tuks.co.za>
2016-08-16 18:14:34 +0200

    Added Angular Material design

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
-var InsuranceProfiling = angular.module('InsuranceProfiling', ['ngRoute','chart.js']);
+var InsuranceProfiling = angular.module('InsuranceProfiling', ['ngRoute','chart.js','ngMaterial']);
+
 // configure our routes
 InsuranceProfiling.config(function($routeProvider) {
 	$routeProvider
@@ -65,33 +66,62 @@ InsuranceProfiling.controller('signupController', function($scope) {
 });
 
 InsuranceProfiling.controller('graphController', function($scope) {
-	$scope.message = '';
-	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-	$scope.series = ['Series A', 'Series B'];
-	$scope.data = [
-		[65, 59, 80, 81, 56, 55, 40],
-		[28, 48, 40, 19, 86, 27, 90]
-	];
-	$scope.onClick = function (points, evt) {
-		console.log(points, evt);
-	};
-	$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-	$scope.options = {
-		scales: {
-			yAxes: [
-				{
-					id: 'y-axis-1',
-					type: 'linear',
-					display: true,
-					position: 'left'
-				},
-				{
-					id: 'y-axis-2',
-					type: 'linear',
-					display: true,
-					position: 'right'
-				}
-			]
-		}
-	};
+	$scope.message = 'This is the Graph page (passed from controller as message )';
+});
+
+
+InsuranceProfiling.controller("BubbleCtrl",
+  function ($scope) {
+    $scope.series = ['Lelelelele A', 'Series B'];
+
+    $scope.data = [
+      [{
+        x: 25,
+        y: 24,
+        r: 50
+      }],
+      [{
+        x: 23,
+        y: 20,
+        r: 50
+      }]
+    ];
+});
+
+InsuranceProfiling.controller("PieCtrl",
+  function ($scope) {
+  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  $scope.data = [300, 500, 100];
+});
+
+InsuranceProfiling.controller("LineCtrl",
+  function ($scope) {
+		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+		$scope.series = ['Series A', 'Series B'];
+		$scope.data = [
+			[65, 59, 80, 81, 56, 55, 40],
+			[28, 48, 40, 19, 86, 27, 90]
+		];
+		$scope.onClick = function (points, evt) {
+			console.log(points, evt);
+		};
+		$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+		$scope.options = {
+			scales: {
+				yAxes: [
+					{
+						id: 'y-axis-1',
+						type: 'linear',
+						display: true,
+						position: 'left'
+					},
+					{
+						id: 'y-axis-2',
+						type: 'linear',
+						display: true,
+						position: 'right'
+					}
+				]
+			}
+		};
 });
