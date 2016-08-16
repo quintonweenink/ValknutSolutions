dd8b1f57dd572bf9a41f51a6198a9ba9219529ce
Charl Jansen van Vuuren <u13054903@tuks.co.za>
2016-08-16 22:41:39 +0200

    Added Analyst filter page with better graph example

--- public/js/script.js
+++ public/js/script.js
@@ -39,6 +39,11 @@ InsuranceProfiling.config(function($routeProvider) {
 		templateUrl : 'views/pages/graph.html',
 		controller  : 'graphController'
 	})
+	.when('/filter',
+	{
+		templateUrl : 'views/pages/filter.html',
+		controller : 'filterController'
+	})
 
 	;
 });
@@ -65,77 +70,108 @@ InsuranceProfiling.controller('signupController', function($scope) {
 	$scope.message = '';
 });
 
+InsuranceProfiling.controller('filterController',function($scope)
+{
+	$scope.message = '';
+	$scope.data = {};
+	$scope.data.cb1 = false;
+	$scope.data.cb2 = false;
+
+	$scope.submit = function(){
+
+		if($scope.data.cb1==true && $scope.data.cb2==false)
+		{
+			$scope.labels = ["Age"];
+			$scope.data = [20]
+		}
+
+		if($scope.data.cb1==false && $scope.data.cb2==true)
+		{
+			$scope.labels = ["Location"];
+			$scope.data = [20];
+		}
+
+		if($scope.data.cb1==true && $scope.data.cb2==true)
+		{
+			$scope.labels = ["Age","Location"];
+			$scope.data = [20,20];
+
+		}
+
+	};
+});
+
 InsuranceProfiling.controller('graphController', function($scope) {
 	$scope.message = 'This is the Graph page (passed from controller as message )';
 });
 
 
 InsuranceProfiling.controller('SelectedTextController', function($scope) {
-      $scope.items = ["Location", "Age"];
-      $scope.selectedItem;
-      $scope.getSelectedText = function() {
-        if ($scope.selectedItem !== undefined) {
-          return "You have selected:" + $scope.selectedItem;
-        } else {
-          return "Please select a filter";
-        }
-      };
-    });
+	$scope.items = ["Location", "Age"];
+	$scope.selectedItem;
+	$scope.getSelectedText = function() {
+		if ($scope.selectedItem !== undefined) {
+			return "You have selected:" + $scope.selectedItem;
+		} else {
+			return "Please select a filter";
+		}
+	};
+});
 
- InsuranceProfiling.controller("PieCtrl",
-		   function ($scope) {
-						$scope.labels = ["Age", "Time"];
-						$scope.data = [20, 100];
-	 });
+InsuranceProfiling.controller("PieCtrl",
+function ($scope) {
+	$scope.labels = ["Age", "Time"];
+	$scope.data = [20, 100];
+});
 
 InsuranceProfiling.controller("BubbleCtrl",
-  function ($scope) {
-    $scope.series = ['Lelelelele A', 'Series B'];
+function ($scope) {
+	$scope.series = ['Lelelelele A', 'Series B'];
 
-    $scope.data = [
-      [{
-        x: 25,
-        y: 24,
-        r: 50
-      }],
-      [{
-        x: 23,
-        y: 20,
-        r: 50
-      }]
-    ];
+	$scope.data = [
+		[{
+			x: 25,
+			y: 24,
+			r: 50
+		}],
+		[{
+			x: 23,
+			y: 20,
+			r: 50
+		}]
+	];
 });
 
 
 
 InsuranceProfiling.controller("LineCtrl",
-  function ($scope) {
-		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-		$scope.series = ['Series A', 'Series B'];
-		$scope.data = [
-			[65, 59, 80, 81, 56, 55, 40],
-			[28, 48, 40, 19, 86, 27, 90]
-		];
-		$scope.onClick = function (points, evt) {
-			console.log(points, evt);
-		};
-		$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-		$scope.options = {
-			scales: {
-				yAxes: [
-					{
-						id: 'y-axis-1',
-						type: 'linear',
-						display: true,
-						position: 'left'
-					},
-					{
-						id: 'y-axis-2',
-						type: 'linear',
-						display: true,
-						position: 'right'
-					}
-				]
-			}
-		};
+function ($scope) {
+	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+	$scope.series = ['Series A', 'Series B'];
+	$scope.data = [
+		[65, 59, 80, 81, 56, 55, 40],
+		[28, 48, 40, 19, 86, 27, 90]
+	];
+	$scope.onClick = function (points, evt) {
+		console.log(points, evt);
+	};
+	$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+	$scope.options = {
+		scales: {
+			yAxes: [
+				{
+					id: 'y-axis-1',
+					type: 'linear',
+					display: true,
+					position: 'left'
+				},
+				{
+					id: 'y-axis-2',
+					type: 'linear',
+					display: true,
+					position: 'right'
+				}
+			]
+		}
+	};
 });
