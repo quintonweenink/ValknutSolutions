92cd970712802ec67c5bb68c24f6b7155cd8a34b
QuintonWeenink <qweenink@gmail.com>
2016-08-29 22:33:57 +0200

    Added bcrypt to the package.json and added additional features to the user model

--- DBControllers/UserController.js
+++ DBControllers/UserController.js
@@ -1,35 +1,37 @@
 var models = require("../models");
 var express = require('express');
+var bcrypt = require('bcrypt');
 var user_id;
 module.exports = {
-createUser : function createUser(user, callback)
-{
-	models.User.findOrCreate({
-		where: {
-			firstName : user.first_name,
-			lastName : user.last_name,
-			phoneNumber : user.phone_number,
-			maritalStatus : user.marital_status,
-			dateOfBirth : user.date_of_birth,
-			gender : user.gender,
-			city : user.city,
-			email : user.email
-		},
-		defaults : {}
-	})
-	.spread(function(new_user, created){
-		//console.log("New user :" + JSON.stringify(new_user.get({plain:true})));
-		//console.log("Created field: " + JSON.stringify(created));
-		callback(new_user.id);
-		return new_user.id;
-	});
-},
-deleteUserByID : function deleteUserByID(id)
-{
-  var delete_user = models.User.destroy({
-    where: {
-    id: id
-  }});
-  return delete_user;
-}
+	createUser : function createUser(user, callback)
+	{
+		newUser.password = hash;
+		models.User.findOrCreate({
+			where: {
+				firstName : user.first_name,
+				lastName : user.last_name,
+				phoneNumber : user.phone_number,
+				maritalStatus : user.marital_status,
+				dateOfBirth : user.date_of_birth,
+				gender : user.gender,
+				city : user.city,
+				email : user.email
+			},
+			defaults : {}
+		})
+		.spread(function(new_user, created){
+			//console.log("New user :" + JSON.stringify(new_user.get({plain:true})));
+			//console.log("Created field: " + JSON.stringify(created));
+			callback(new_user.id);
+			return new_user.id;
+		});
+	},
+	deleteUserByID : function deleteUserByID(id)
+	{
+	  var delete_user = models.User.destroy({
+	    where: {
+	    id: id
+	  }});
+	  return delete_user;
+	}
 };
