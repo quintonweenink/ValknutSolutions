a5ce51105da8144428d46447baa91906dc5efc83
u13054903 <u13054903@tuks.co.za>
2016-09-06 14:25:30 +0200

    Added backend graphing API and updated front end graphs Refactored all graph functionality Added Line chart with Signups over Time graph

--- routes/graph.js
+++ routes/graph.js
@@ -6,8 +6,11 @@ var userController = require("../DBControllers/UserController");
 
 
 module.exports = function(app, passport){
-//TODO: 
-	app.route('/api/graph/Location')
+	app.route('/api/graph/location')
+	.get(function(req,res){
+		res.json("Please supply a chart type: eg. /location/pie");
+	});
+	app.route('/api/graph/location/:type')
 	.get(function(req, res) {
 		models.User.findAll().then(function(users){
 			var cities = [];
@@ -29,25 +32,351 @@ module.exports = function(app, passport){
 			var GraphObject = new Object;
 			GraphObject.labels = cities;
 			GraphObject.data = cityCount;
+			if(req.params.type == "pie")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Location"
+					},
+					legend: {
+						display: true,
+						labels: {
+							fontColor: 'rgb(255, 99, 132)'
+						}
+					}
+				};
+			}
+			else if(req.params.type == "bar")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Location"
+					},
+					scales: {
+						yAxes: [{
+							display: true,
+							ticks: {
+								suggestedMin: 0    // minimum will be 0, unless there is a lower value.
+							}
+						}]
+					}
+				};
+			}
+			else
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Location"
+					}
+				}
+			}
+			res.json(GraphObject);
+		});
+	});
+
+	function getAge(dateString)
+	{
+		var today = new Date();
+		var birthDate = new Date(dateString);
+		var age = today.getFullYear() - birthDate.getFullYear();
+		var m = today.getMonth() - birthDate.getMonth();
+		if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
+		{
+			age--;
+		}
+		return age;
+	}
+
+	app.route('/api/graph/age')
+	.get(function(req,res){
+		res.json("Please supply a chart type: eg. /age/pie");
+	});
+
+	app.route('/api/graph/age/:type')
+	//User Post route
+	.get(function(req, res) {
+		models.User.findAll().then(function(users){
+			var ages = [];
+			var ageCount = [];
+			for (var i = 0; i < users.length; i++)
+			{
+				var user = users[i];
+				var Age = getAge(user.dateOfBirth);
+				if(ages.indexOf(Age) == -1)
+				{
+					ages.push(Age);
+					ageCount.push(1);
+				}
+				else
+				{
+					ageCount[ages.indexOf(Age)]++;
+				}
+			}
+			var GraphObject = new Object;
+			GraphObject.labels = ages;
+			GraphObject.data = ageCount;
+			if(req.params.type == "pie")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Age"
+					},
+					legend: {
+						display: true,
+						labels: {
+							fontColor: 'rgb(255, 99, 132)'
+						}
+					}
+				};
+			}
+			else if(req.params.type == "bar")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Age"
+					},
+					scales: {
+						yAxes: [{
+							display: true,
+							ticks: {
+								suggestedMin: 0    // minimum will be 0, unless there is a lower value.
+							}
+						}]
+					}
+				};
+			}
+			else
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Age"
+					}
+				}
+			}
+			res.json(GraphObject);
+		});
+	});
+
+	app.route('/api/graph/marital')
+	.get(function(req,res){
+		res.json("Please supply a chart type: eg. /marital/pie");
+	});
+
+	app.route('/api/graph/marital/:type')
+	//User Post route
+	.get(function(req, res) {
+		models.User.findAll().then(function(users){
+			var maritalStatus = [];
+			var maritalCount = [];
+			for (var i = 0; i < users.length; i++)
+			{
+				var user = users[i];
+				var Status = user.maritalStatus.toUpperCase();
+				if(maritalStatus.indexOf(Status) == -1)
+				{
+					maritalStatus.push(Status);
+					maritalCount.push(1);
+				}
+				else
+				{
+					maritalCount[maritalStatus.indexOf(Status)]++;
+				}
+			}
+			var GraphObject = new Object;
+			GraphObject.labels = maritalStatus;
+			GraphObject.data = maritalCount;
+			if(req.params.type == "pie")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Marital Status"
+					},
+					legend: {
+						display: true,
+						labels: {
+							fontColor: 'rgb(255, 99, 132)'
+						}
+					}
+				};
+			}
+			else if(req.params.type == "bar")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Marital Status"
+					},
+					scales: {
+						yAxes: [{
+							display: true,
+							ticks: {
+								suggestedMin: 0    // minimum will be 0, unless there is a lower value.
+							}
+						}]
+					}
+				};
+			}
+			else
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Marital Status"
+					}
+				}
+			}
+			res.json(GraphObject);
+		});
+	});
+
+	app.route('/api/graph/gender')
+	.get(function(req,res){
+		res.json("Please supply a chart type: eg. /gender/pie");
+	});
+
+	app.route('/api/graph/gender/:type')
+	//User Post route
+	.get(function(req, res) {
+		models.User.findAll().then(function(users){
+			var gender = [];
+			var genderCount = [];
+			for (var i = 0; i < users.length; i++)
+			{
+				var user = users[i];
+				var Status = user.gender.toUpperCase();
+				if(gender.indexOf(Status) == -1)
+				{
+					gender.push(Status);
+					genderCount.push(1);
+				}
+				else
+				{
+					genderCount[gender.indexOf(Status)]++;
+				}
+			}
+			var GraphObject = new Object;
+			GraphObject.labels = gender;
+			GraphObject.data = genderCount;
+			if(req.params.type == "pie")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Gender"
+					},
+					legend: {
+						display: true,
+						labels: {
+							fontColor: 'rgb(255, 99, 132)'
+						}
+					}
+				};
+			}
+			else if(req.params.type == "bar")
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Gender"
+					},
+					scales: {
+						yAxes: [{
+							display: true,
+							ticks: {
+								suggestedMin: 0    // minimum will be 0, unless there is a lower value.
+							}
+						}]
+					}
+				};
+			}
+			else
+			{
+				GraphObject.options =
+				{
+					title: {
+						display: true,
+						text:  "Gender"
+					}
+				}
+			}
+			res.json(GraphObject);
+		});
+	});
+
+	function getMonths(dateString)
+	{
+		var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+	];
+		var birthDate = new Date(dateString);
+		var month = birthDate.getMonth()
+		return monthNames[month];
+	}
+
+	app.route('/api/graph/signups/')
+	//User Post route
+	.get(function(req, res) {
+		models.User.findAll().then(function(users){
+			var months = ["January", "February", "March", "April", "May", "June",
+	  "July", "August", "September", "October", "November", "December"
+		];
+		var monthCount = [];
+			for(var j=0 ;j < months.length;j++)
+			  	monthCount[j] = 0;
+
+			for (var i = 0; i < users.length; i++)
+			{
+				var user = users[i];
+				var Status = getMonths(user.createdAt);
+					monthCount[months.indexOf(Status)]++;
+			}
+
+
+
+			var GraphObject = new Object;
+			GraphObject.labels = months;
+			GraphObject.data = monthCount;
+				GraphObject.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-1' }];
+				GraphObject.options = {
+					title: {
+						display: true,
+						text:  "Signups per month"
+					},
+					scales: {
+						yAxes: [
+							{
+								id: 'y-axis-1',
+								type: 'linear',
+								display: true,
+								position: 'left'
+							}
+						]
+					}
+				};
 
 			res.json(GraphObject);
 		});
 	});
 
-	app.route('/api/graph/Age')
-	//User Post route
-	.get(function(req, res) {
-		models.User.findAll().then(function(users){
-			for (var i = 0; i < users.length; i++)
-			{
-				var user = users[i];
-				var Age = user.dateOfBirth;
-		//	var Age = getAge(user.dateOfBirth);
-			console.log(users);
-			}
-			res.json(Age);
-		});
-	});
+
 
 
 
