683dc185bf0ab18001a68063f1d1ace84efcbd36
QuintonWeenink <qweenink@gmail.com>
2016-09-08 23:13:35 +0200

    Added basic users to the the live feed for representation

--- public/js/LiveUserFeedController.js
+++ public/js/LiveUserFeedController.js
@@ -1,11 +1,15 @@
 InsuranceProfiling.controller('LiveUserFeedController', function($scope, $http) {
 	$scope.message = "No message";
-	$scope.users;
+	$scope.users = [
+		{'first_name': 'Quinton', 'last_name':'Weenink'},
+		{'first_name': 'Margo', 'last_name':'Henning'}
+	];
 
+	/*Postponed
     var socket = io('http://localhost');
 
     socket.on('new user', function (data) {
       $scope.users.push(data);
-    });
+  });*/
 
 })
