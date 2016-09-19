d1a25756861f95765bb3ac643283107772a9effa
QuintonWeenink <qweenink@gmail.com>
2016-09-19 12:59:54 +0200

    no message

--- routes/fbMessenger.js
+++ routes/fbMessenger.js
@@ -91,7 +91,7 @@ function callSendAPI(messageData) {
     } else {
       console.error("Unable to send message.");
       console.error(response);
-      console.error(error.message);
+      //console.error(error.message);
     }
   });
 }
