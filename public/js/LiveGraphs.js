9fd0acc533e191cc38990fe6d883f6e43e24a7ab
u13054903 <u13054903@tuks.co.za>
2016-09-28 18:27:26 +0200

    Fixed bug #115

--- public/js/LiveGraphs.js
+++ public/js/LiveGraphs.js
@@ -2,7 +2,7 @@ InsuranceProfiling.controller('LiveGraphController',function($scope, $http)
 {
 	$http.get("/api/graph/signups/")
 	.then(function(response){
-		$scope.data_line = response.data.data;
+		$scope.data_line = [ response.data.data ];
 		$scope.series_line = response.data.labels;
 		$scope.labels_line = response.data.labels;
 		$scope.options_line = response.data.options;
@@ -22,7 +22,7 @@ function updateChart(response)
 {
 	//$scope.$destroy();
 	console.log("Updating Chart ");
-	$scope.data_line = response.data.data;
+	$scope.data_line = [ response.data.data ];
 	$scope.message = response.data.data;
 	$scope.series_line = response.data.labels;
 	$scope.labels_line = response.data.labels;
