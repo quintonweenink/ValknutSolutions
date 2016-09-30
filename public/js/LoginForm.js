88f46b671ab2e7c45bf5049fe106f55da608799a
QuintonWeenink <qweenink@gmail.com>
2016-09-30 21:40:34 +0200

    Added an interface for getting a access token

--- public/js/LoginForm.js
+++ public/js/LoginForm.js
@@ -1,3 +1,29 @@
-InsuranceProfiling.controller('loginFormController', function($scope) {
-	$scope.message = 'Error will be shown here my friends';
+InsuranceProfiling.controller('loginFormController', function($scope, $rootScope, $http) {
+	$scope.cookie = getCookie()
+
+	$http.get("/api/user?token="+$rootScope.token)
+	.then(function(users){
+		$scope.message = 'You already have a token'
+	});
+
+	console.log($scope.message)
+	$scope.analyst;
+	$scope.postForm = function()
+	{
+		$http({
+	    method: 'POST',
+	    url: '/api/analyst/authenticate',
+	    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	    data:
+		'email='+$scope.analyst.email+
+		'&password='+$scope.analyst.password
+		}).success(function (res) {
+			$scope.message = res.message
+			if(res.success){
+				setCookie(res.token)
+				console.log(res.token)
+			}
+			//window.location = '/';
+		})
+	}
 });
