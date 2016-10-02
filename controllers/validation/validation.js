3385821b4651d2b9f0b5df82b1e71c057078bfa1
QuintonWeenink <qweenink@gmail.com>
2016-10-02 21:43:32 +0200

    new file to git ignore

---
+++ controllers/validation/validation.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var jwt = require('jsonwebtoken')
+
+var auth = require('../../config/auth')
+
+const config = require('../../config/auth')
+
+module.exports = {
+
+    validate : function(user){
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+        }
+    }
+
+}
