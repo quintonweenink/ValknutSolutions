e764cf3e4ee34a33b9c26ba08f5a910ebca7968e
QuintonWeenink <qweenink@gmail.com>
2016-10-02 21:43:32 +0200

    new file to git ignore

--- config/messageList.js
+++ config/messageList.js
@@ -1,6 +1,20 @@
 module.exports = {
-    8:'You will be contacted shortly...',
-    0:'Please reply with your Name:',
+    8:
+    {
+        message:'You will be contacted shortly...',
+        validate: function(res){
+            //nothing to validate
+            return true
+        }
+    },
+    0:
+    {
+        message:'Please reply with your Name:',
+        validate: function(res){
+            //validate is a string (no numbers and symbols*)
+            return true
+        }
+    },
     1:'Please reply with your Last name:',
     2:'Please reply with your Phone number:',
     3:'Please reply with your Marital status:',
