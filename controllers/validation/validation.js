4d9d16e53f2fd05977c1f8894808f479028af59a
QuintonWeenink <qweenink@gmail.com>
2016-10-02 21:43:32 +0200

    new file to git ignore

--- controllers/validation/validation.js
+++ controllers/validation/validation.js
@@ -7,6 +7,7 @@ var auth = require('../../config/auth')
 const config = require('../../config/auth')
 
 module.exports = {
+<<<<<<< 2da2d4c299e655eda25c5380136ea90c621ef1f5
 	isName : function(res){
 		return false
 	},
@@ -16,4 +17,13 @@ module.exports = {
     isEmail : function(res){
 		return false
     }
+=======
+
+    validate : function(user){
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+        }
+    }
+
+>>>>>>> new file to git ignore
 }
