6ebd5b7f7d260eedeb3bda811d8b14b443931147
QuintonWeenink <qweenink@gmail.com>
2016-10-03 20:30:34 +0200

    Added more test

--- controllers/validation/validation.js
+++ controllers/validation/validation.js
@@ -7,11 +7,13 @@ var auth = require('../../config/auth')
 const config = require('../../config/auth')
 
 module.exports = {
-
-    validate : function(user){
-        for (var attr in obj) {
-            if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
-        }
+	isName : function(res){
+		return false
+	},
+	isPhoneNumber : function(res){
+		return false
+    },
+    isEmail : function(res){
+		return false
     }
-
 }
