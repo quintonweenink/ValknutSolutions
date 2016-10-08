cbbc197baec6c2dabc903d702e7ec046e4cc3773
QuintonWeenink <qweenink@gmail.com>
2016-10-08 22:14:39 +0200

    This will fail again

--- config/auth.js
+++ config/auth.js
@@ -10,11 +10,12 @@ var authObj = {
       "url": "https://marketlead.herokuapp.com/"
 }
 
+console.log("Node env: " + process.env.NODE_ENV)
 
 if (env != "development"){
   var pg = require('pg');
   pg.defaults.ssl = true;
-  authObj = JSON.parse(process.env.AUTH)
+  authObj = JSON.parse(process.env.AUTH || authObj)
 }
 
 module.exports = authObj
