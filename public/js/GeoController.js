097c713da77511eda81938ec3988060f9bd55757
u13054903 <u13054903@tuks.co.za>
2016-10-09 02:41:22 +0200

    Added Users per Region and Users per Country Needs changes to model for Country to function - Defaults to south africa at the moment

--- public/js/GeoController.js
+++ public/js/GeoController.js
@@ -1,4 +1,3 @@
-
 InsuranceProfiling.controller('geoController', function($scope, $http) {
 	$scope.message = 'This is the Geo page (passed from controller as message )';
 
@@ -7,31 +6,45 @@ InsuranceProfiling.controller('geoController', function($scope, $http) {
 
 	function drawRegionsMap() {
 
-$http.get("/api/graph/Geochart/")
-.then(function(response){
+		$http.get("/api/graph/Geochart/")
+		.then(function(response){
+			// var data = google.visualization.arrayToDataTable([
+			// 	['City',   'Users'],
+			// 	['Pretoria', 20],
+			// 	['PE', 30],
+			// 	['Eastern Cape', 40],
+			// 	['Northen Cape',50],
+			// 	['North West', 60],
+			// 	['KwaZulu-Natal', 70],
+			// 	['Free State',80]
+			// ]);
+			var data = google.visualization.arrayToDataTable(response.data.data);
 
+			var options = response.data.options;
 
-		// var data = google.visualization.arrayToDataTable([
-		// 	['City',   'Users'],
-		// 	['Pretoria', 20],
-		// 	['PE', 30],
-		// 	['Eastern Cape', 40],
-		// 	['Northen Cape',50],
-		// 	['North West', 60],
-		// 	['KwaZulu-Natal', 70],
-		// 	['Free State',80]
-		// ]);
+			var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-		var data = google.visualization.arrayToDataTable(response.data.data);
+			chart.draw(data, options);
+		});
+	}
 
-		var options = response.data.options;
-		var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-		chart.draw(data, options);
+	google.charts.load('upcoming', {'packages':['geochart']});
+	google.charts.setOnLoadCallback(drawAfrica);
 
-	});
+	function drawAfrica() {
 
+		$http.get("/api/graph/GeoChartCountry/")
+		.then(function(response2){
+
+			var data = google.visualization.arrayToDataTable(response2.data.data);
+
+			var options = response2.data.options;
+
+			var chart2 = new google.visualization.GeoChart(document.getElementById('africa_div'));
+
+			chart2.draw(data, options);
+		});
 	}
 
 
-
-});
+	});
--- routes/dynamicGraphs.js
+++ routes/dynamicGraphs.js
@@ -341,18 +341,22 @@ module.exports =	function(app, passport){
 								arr2[i+1][0] = cities[i];
 								arr2[i+1][1] = cityCount[i];
 							}
-
 						var GraphObject = new Object;
 
 						GraphObject.data = arr2;
 						GraphObject.options =
 						{
 							region: 'ZA',
-							resolution : 'provinces',
+							legend : {textStyle: {color: 'blue', fontSize: 16}},
+							// resolution : 'provinces',
+							backgroundColor: '#86c5da',
+							datalessRegionColor : '#FEE8D6',
+							// height : 600,
+							// keepAspectRatio : true,
 							displayMode: 'markers',
-							colorAxis: {colors: ['green', 'blue','black']},
-							maxSize : 8,
-							minSize : 5
+							markerOpacity : 0.7,
+						 	sizeAxis : { maxSize: 15, minSize: 5},
+							colorAxis: {colors: ['red','green', 'blue']}
 						}
 						res.json(GraphObject);
 					}
@@ -369,6 +373,85 @@ module.exports =	function(app, passport){
 			.delete(function(req, res) {
 				res.json({ message: 'This route does not support delete requests'});
 			});
+
+
+
+			app.route('/api/graph/GeoChartCountry/')
+			.get(function(req,res){
+
+					models.User.findAll().then(function(users){
+						var countries = [];
+						var countryCount = [];
+
+						//TODO: Replace South Africa with user.country
+
+						// for (var i = 0; i < users.length; i++)
+						// {
+						// 	var user = users[i];
+						// 	if(countries.indexOf("user.country") == -1)
+						// 	{
+						// 		countries.push(user.country);
+						// 		countryCount.push(1);
+						// 	}
+						// 	else
+						// 	{
+						// 		countryCount[countries.indexOf(user.country)]++;
+						// 	}
+						// }
+
+						for (var i = 0; i < users.length; i++)
+						{
+							var user = users[i];
+							if(countries.indexOf("South Africa") == -1)
+							{
+								countries.push("South Africa");
+								countryCount.push(1);
+							}
+							else
+							{
+								countryCount[countries.indexOf("South Africa")]++;
+							}
+						}
+
+						var arr2 = new Array();
+						arr2[0] = new Array(2);
+						arr2[0][0] = 'Country';
+						arr2[0][1] = 'Users';
+							for (var i=0; i < countries.length;i++)
+							{
+								arr2[i+1] = new Array(2);
+								arr2[i+1][0] = countries[i];
+								arr2[i+1][1] = countryCount[i];
+							}
+						var GraphObject = new Object;
+
+						GraphObject.data = arr2;
+						GraphObject.options =
+						{
+								region: '002',
+								backgroundColor: '#86c5da',
+								datalessRegionColor : '#FEE8D6',
+								displayMode: 'region',
+								// markerOpacity : 0.7,
+								// sizeAxis : { maxSize: 15, minSize: 5},
+								// colorAxis: {colors: ['red','green', 'blue']}
+						};
+						res.json(GraphObject);
+					}
+
+				);
+
+			})
+			.put(function(req, res) {
+				res.json({ message: 'This route does not support put requests'	});
+			})
+			.post(function(req, res) {
+				res.json({ message: 'This route does not support post requests'	});
+			})
+			.delete(function(req, res) {
+				res.json({ message: 'This route does not support delete requests'});
+			});
+
 
 
 
