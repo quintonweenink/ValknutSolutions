775c31814cb435fd57623149c294ee899cc36c74
QuintonWeenink <qweenink@gmail.com>
2016-10-10 18:11:18 +0200

    Everything working but recurring processes fail

--- public/js/LiveUserFeedController.js
+++ public/js/LiveUserFeedController.js
@@ -4,7 +4,7 @@ InsuranceProfiling.controller('LiveUserFeedController', function($scope, $http, 
 	$scope.users = {}
 	$scope.filters = {}
 	$scope.filters.showProcessed = false
-	$scope.user
+	$scope.user = {}
 	$scope.message = "";
 
 	function calcAge(dateString) {
@@ -61,7 +61,8 @@ InsuranceProfiling.controller('LiveUserFeedController', function($scope, $http, 
          targetEvent: ev,
          templateUrl:'views/pages/processLead.tmpl.html',
          locals: {
-           user: user
+           user: user,
+		   calcAge : $scope.calcAge
          },
 		 clickOutsideToClose:true,
          controller: DialogController,
@@ -75,11 +76,13 @@ InsuranceProfiling.controller('LiveUserFeedController', function($scope, $http, 
 
 	$scope.filterUsers = function filterUsers(users, filters) {
 		var filteredUsers = []
-		for(var x = 0; x < users.length; x++){
-			if(users[x].processed == filters.showProcessed)
-				filteredUsers.push(users[x])
-		}
+		if(users != null)
+			for(var x = 0; x < users.length; x++){
+				if(users[x].processed == filters.showProcessed)
+					filteredUsers.push(users[x])
+			}
 		return filteredUsers
+
 	}
 
 	$scope.changeShowProcessed = function changeShowProcessed() {
@@ -109,6 +112,70 @@ InsuranceProfiling.controller('LiveUserFeedController', function($scope, $http, 
 	  $scope.answer = function(answer) {
 		$mdDialog.hide(answer);
 	  };
+
+	  $scope.calcAge = function(dateString) {
+		  var birthday = +new Date(dateString);
+		  return ~~((Date.now() - birthday) / (31557600000));
+	};
+
+		$scope.drawMap = function drawMap(user){
+			console.log("Drawing the map")
+			console.log(user)
+			var key = 'AIzaSyDQ45U7xOfDtZpgVjhIeIO8h280x9KBYP4'
+			google.charts.load('upcoming', {'packages':['geochart'], mapsApiKey: key })
+			google.charts.setOnLoadCallback(function() {
+				var cities = [];
+				var cityCount = [];
+					if(cities.indexOf(user.city.toUpperCase()) == -1)
+					{
+						cities.push(user.city.toUpperCase());
+						cityCount.push(1);
+					}
+					else
+					{
+						cityCount[cities.indexOf(user.city.toUpperCase())]++;
+					}
+
+				var arr2 = new Array();
+				arr2[0] = new Array(2);
+				arr2[0][0] = 'City';
+				arr2[0][1] = 'Users';
+					for (var i=0; i < cities.length;i++)
+					{
+						arr2[i+1] = new Array(2);
+						arr2[i+1][0] = cities[i];
+						arr2[i+1][1] = cityCount[i];
+					}
+				var GraphObject = new Object;
+
+				GraphObject.data = arr2;
+				GraphObject.options =
+				{
+					region: 'ZA',
+					legend : {textStyle: {color: 'blue', fontSize: 16}},
+					// resolution : 'provinces',
+					backgroundColor: '#86c5da',
+					datalessRegionColor : '#FEE8D6',
+					// height : 600,
+					// keepAspectRatio : true,
+					displayMode: 'markers',
+					markerOpacity : 0.7,
+					sizeAxis : { maxSize: 15, minSize: 5},
+					colorAxis: {colors: ['red','green', 'blue']}
+				}
+
+
+					var data = google.visualization.arrayToDataTable(GraphObject.data);
+
+					var options = GraphObject.options;
+
+					var chart = new google.visualization.GeoChart(document.getElementById('regions_div')[0])
+
+					chart.draw(data, options);
+			})
+		}
+
+
 
   	$scope.process = makeProcessed;
 }
