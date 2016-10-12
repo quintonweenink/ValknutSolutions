3709636c2c57691ec333a9f26860ecc2dd6c8a4e
QuintonWeenink <qweenink@gmail.com>
2016-10-12 10:46:08 +0200

    Fished up hashing

--- controllers/hashing/hashing.js
+++ controllers/hashing/hashing.js
@@ -2,19 +2,7 @@
 
 var crypto = require('crypto')
 
-const generateToken = require('../auth/auth').generateToken
 
-var genRandomString = };
-
-var sha512 = function(password, salt){
-    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-    hash.update(password);
-    var value = hash.digest('hex');
-    return {
-        salt:salt,
-        passwordHash:value
-    };
-};
 
 module.exports = {
 
